edabc9a76a2148f0095872a2448c68d0c0289f7c
A <annett-online@ya.ru>
2024-11-10 01:06:18 +0300

    change positions

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express ()
 const  PORT = 8000
 
 app.get ( '/' , ( req, res ) => {   res. send ( ' Hello World' ) });
-app.get ( '/about' , ( req, res ) => {   res. send ( 'Об ужасном бэкенде 🎉' );  res. send ( 'О хорошем бэкенде 🎉' )});
+app.get ( '/about' , ( req, res ) => {     res. send ( 'О хорошем бэкенде 🎉' ); res. send ( 'Об ужасном бэкенде 🎉' )});
 app.listen ( PORT , () => { console.log ( `✅ Сервер работает на порту $ { PORT } ` ) ; })
 
 
