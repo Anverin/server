eaedabd845faba7c7e691d492503d89c0d459eef
A <annett-online@ya.ru>
2024-11-10 00:57:01 +0300

    remove spaces

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express ()
 const  PORT = 8000
 
 app.get ( '/' , ( req, res ) => {   res. send ( ' Hello World' ) });
-app.get ( '/ about ' , ( req, res ) => {   res. send ( 'О маршруте 🎉' ) });
+app.get ( '/about' , ( req, res ) => {   res. send ( 'О маршруте 🎉' ) });
 app.listen ( PORT , () => { console.log ( `✅ Сервер работает на порту $ { PORT } ` ) ; })
 
 
